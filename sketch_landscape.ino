174a698773b75cb06098f408ca45f843d792732f
Leo Xiong <39155356+XZLeo@users.noreply.github.com>
2018-12-06 14:29:23 +0100

    Only for the use of test landscape judge
    1.先测试sketch_nova26确定好relfectance sensor连接的顺序，是不是Arduino 3—8 连接 传感器1-6？
    2.sensorValues0—5是不是传感器1-6？影响到判断左右的函数
    3.先测试直行函数，确定后轮是否能用？
    4.记录面包板上如何连线？

--- sketch_landscape.ino
+++ sketch_landscape.ino
@@ -2,14 +2,16 @@
  容纳graspping system的控制函数和line following的控制函数
  */
 #include <Servo.h>
+#include <HCSR04.h>
 #include <QTRSensors.h> // Pololu QTR Sensor Library.
 
 Servo left_motor, right_motor;
 Servo gripper, lifter;
-const int lifterPin = 9;
-const int gripperPin = 10;
+const int TrigPin = 9;
+const int EchoPin = 10;
 const int left_motorPin = 11;
 const int right_motorPin = 12;
+UltraSonicDistanceSensor distanceSensor(TrigPin, EchoPin);
 
 #define KP 0.2 //experiment to determine this, start by something sm  all that just makes your bot follow the line at a slow speed
 #define KD 0.5 //experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
@@ -24,7 +26,7 @@ const int right_motorPin = 12;
 #define DEBUG 0               // 设置为1后，方便后面degbug时要不要打印出来传感器等的参数      
 
 // sensors are connected to digital pins 2 to 7
-QTRSensorsRC qtrrc((unsigned char[]) {2, 3, 4, 5, 6, 7},
+QTRSensorsRC qtrrc((unsigned char[]) { 3, 4, 5, 6, 7, 8},  // 3号传感器对应最左边or最右边要测清楚？
   NUM_SENSORS, TIMEOUT, EMITTER_PIN);
 
 unsigned int sensorValues[NUM_SENSORS];
@@ -32,55 +34,13 @@ int lastError = 0;
 
 void setup() {
   left_motor.attach(left_motorPin);
-  right_motor.attach(right_motorPin);
-  gripper.attach(gripperPin);
-  lifter.attach(lifterPin);
-  set_motors(90, 90); // set the motors speed zero.
-  init_gripper();
-  init_lifter();
+  right_motor.attach(right_motorPin);.
   manual_calibration();
 }
 
 void loop() {
-  int position = qtrrc.readLine(sensorValues); 
-  //get calibrated readings along with the line position, refer to the QTR Sensors Arduino Library for more details on line position.
-
-  if (DEBUG) print_sensor_values(position);
-
-  int error = position - 2500;
-  
-  if (DEBUG) print_error(error);
-
-  int motorSpeed = KP * error + KD * (error - lastError);
-  
-  if (DEBUG) print_motorSpeed(motorSpeed);
-
-  lastError = error;
-
-  int leftMotorSpeed = 180 - left_motor_default_speed - motorSpeed; // closer to 0, speed up
-  int rightMotorSpeed = right_motor_default_speed - motorSpeed; // closer to 180, speed up, so slow down here
-  
-  if (DEBUG) print_left_right_motorSpeed(leftMotorSpeed, rightMotorSpeed);
-
-  // set motor speeds using the two motor speed variables above
-  // limit their speed to 90~180
-  set_motors(leftMotorSpeed, rightMotorSpeed);
-  //set_motors(90, 90);  // stop 
- 
  Serial.print(landscape());
 }
-
-void set_motors(int left_motorspeed, int right_motorspeed) {
-  // limit motor's speed to 0~90, 90~180
-  if (180 - left_motorspeed > left_motor_max_speed ) left_motorspeed = 180 - left_motor_max_speed;  // if speed is negative, use 0 instead
-  if (right_motorspeed > right_motor_max_speed ) right_motorspeed = right_motor_max_speed;
-  if ( left_motorspeed > 90) left_motorspeed = 90;   // avoid spinning backward
-  if (right_motorspeed < 90) right_motorspeed = 90;
-  //left_motorspeed = 180 - left_motorspeed;
-  left_motor.write(left_motorspeed);
-  right_motor.write(right_motorspeed);
-}
-
 
 void manual_calibration() {
   //calibrate for sometime by sliding the sensors across the line,
@@ -112,59 +72,6 @@ void manual_calibration() {
   // delay(1000);
 }
 
-void print_sensor_values(int position){
-    // for the use of calibration and debug
-    // print the sensor values as numbers from 0 to 1000, where 0 means maximum reflectance and
-    // 1000 means minimum reflectance, followed by the line position
-    for (unsigned char i = 0; i < NUM_SENSORS; i++){
-      Serial.print(sensorValues[i]);
-      Serial.print('\t');
-    }
-    //Serial.println(); // uncomment this line if you are using raw values
-    String pos = "Position: ";
-    String fpos = pos + position;
-    Serial.println(position); // comment this line out if you are using raw values
-    return;
-}
-
-void print_error(int error){
-    // for the use of debug
-    String err = "Error: ";
-    String ferr = err + error;
-    Serial.println(ferr);
-    Serial.println();
-    return;
-}
-
-void print_motorSpeed(int motorSpeed){
-  Serial.println(motorSpeed);
-  return;
-}
-
-void print_left_right_motorSpeed(int leftMotorSpeed, int rightMotorSpeed){
-  Serial.println(leftMotorSpeed);
-  Serial.println(rightMotorSpeed);  
-  return;
-}
-
-bool check_reflectance_dead_end(int position){
-  int lower_bound = 2300;
-  int higher_bound = 2700;
-  if (position > 2300 and position < 2700){
-    return true;
-  }
-  return false;
-}
-
-void check_u_turn(int position){
-  u_turn = false; // we assume we shouldn't do u-turn
-  // stage1: check for reflectance bounds in a fixed range (determined by testing)
-  u_turn = check_reflectance_dead_end(position);
-  // stage2: check for ultra-sound sensor (maybe a variable that has been set before)
-
-  return 7;  // 可以返回函数？？？
-}
-
 int landscape(){
   // first make sure the car is online, then judge the landscape
   // use ultra sound sensors to distinguish 'only right' and 'right or straight'
@@ -172,7 +79,7 @@ int landscape(){
   //1, 2, 3, 4, 5, 6 corresonds to leftonly, rightonly, T, 十, leftandstraight,
   //rightandstraight 
   if(online_judge()){
-    delay(1000); // experiment to determine this
+    delay(1000); // experiment to determine this，实际中这里要不要delay需要商量
     //这里的逻辑需要重新考虑一下，有坑能出现false全部return到第一个里面
     if(left_judge()){
       if (wal_judge()) return 1;
@@ -292,4 +199,10 @@ bool T_judge(){
 bool wall_judge(){
   // use ultra_sound to judge if there is a wall in fornt
   // return true if there's a wall in the front  
+    double distance = distanceSensor.measureDistanceCm();   
+    if(distance<20)
+      return true;
+    else
+      return false;
+     delay(200);
   }
